a73a360f8b9389310fec00a1f96a42b428980dfd
Kewei12345 <ipc00045@student.bxcl.edu.my>
2023-10-25 14:50:30 +0800

    dict

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+EM = {}
+
+EM["hello"] = "selamat"
+EM["car"] = "kereta"
+EM["house"] = "rumah"
+
+EM2 = {"eat": "makan",
+       "drink": "minum"}
+
+EM["night"] = "malam"
+
+print(f"{EM}\n{EM2}")
